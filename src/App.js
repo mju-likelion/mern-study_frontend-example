5a59098d0641a57ccf55474fb5e4c70a4686ad88
soultree-fly <yuyaebean@gmail.com>
2021-01-02 14:59:42 +0900

    0.2 Set tailwindcss w/ postcss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div />;
+  return (
+    <div className="bg-green-300">
+      <p className="text-base">Haha</p>
+      <p className="text-lg">Oh!</p>
+    </div>
+  );
 }
 
 export default App;
