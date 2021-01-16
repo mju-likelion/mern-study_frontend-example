a8d9fe00e8d685ef7aa9f4e84c3626cd50fa6e48
soultree-fly <yuyaebean@gmail.com>
2021-01-16 14:50:43 +0900

    1.3 Create Auth.login

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -2,29 +2,56 @@ import axios from 'axios';
 import { useState } from 'react';
 
 function Auth() {
+  const [type, setType] = useState('register');
+
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [successMessage, setSuccessMessage] = useState('');
   const [errorMessage, setErrorMessage] = useState('');
 
+  function toggleAuth() {
+    if (type === 'register') setType('login');
+    else setType('register');
+  }
+
   async function handleSubmit(e) {
     e.preventDefault();
-    try {
-      await axios.post('http://localhost:3000/auth/register', {
-        username,
-        email,
-        password,
-      });
-      setSuccessMessage(`Hello ${username}`);
-    } catch (error) {
-      const { status } = error.response;
-      if (status === 400) {
-        setErrorMessage('Bad Request');
-      } else if (status === 409) {
-        setErrorMessage('Conflict');
-      } else {
-        setErrorMessage('Server Error');
+
+    if (type === 'register') {
+      try {
+        await axios.post('/auth/register', {
+          username,
+          email,
+          password,
+        });
+        setSuccessMessage(`Hello ${username}`);
+      } catch (error) {
+        const { status } = error.response;
+        if (status === 400) {
+          setErrorMessage('Bad Request');
+        } else if (status === 409) {
+          setErrorMessage('Conflict');
+        } else {
+          setErrorMessage('Server Error');
+        }
+      }
+    } else {
+      try {
+        const { data } = await axios.post('/auth/login', {
+          email,
+          password,
+        });
+        console.log(data);
+      } catch (error) {
+        const { status } = error.response;
+        if (status === 400) {
+          setErrorMessage('Bad Request');
+        } else if (status === 401) {
+          setErrorMessage('Wrong Email or Password');
+        } else {
+          setErrorMessage('Server Error');
+        }
       }
     }
   }
@@ -46,14 +73,19 @@ function Auth() {
   return (
     <>
       <h1>Register</h1>
+      <button type="button" onClick={toggleAuth}>
+        {type === 'register' ? '로그인하기' : '회원가입하기'}
+      </button>
       <form onSubmit={handleSubmit}>
-        <input
-          name="username"
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={handleChange}
-        />
+        {type === 'register' && (
+          <input
+            name="username"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={handleChange}
+          />
+        )}
         <input
           name="email"
           type="email"
@@ -68,7 +100,9 @@ function Auth() {
           value={password}
           onChange={handleChange}
         />
-        <button type="submit">Submit</button>
+        <button type="submit">
+          {type === 'register' ? 'Register' : 'Login'}
+        </button>
       </form>
       <p className="text-green-400">{successMessage}</p>
       <p className="text-red-400">{errorMessage}</p>
